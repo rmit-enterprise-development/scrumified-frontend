6c431297829f18486d13f3081dd4aeed70010865
Andrew Le Nguyen <truongan1730@gmail.com>
2022-03-16 10:40:20 +0700

    0. first commit

---
+++ components/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DarkModeSwitch from '../components/DarkModeSwitch';
+
+const Container = ({ children }) => {
+  return (
+    <>
+      <DarkModeSwitch />
+    </>
+  );
+};
+
+export default Container;
---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  useColorMode,
+} from '@chakra-ui/react';
+import customTheme from '../styles/theme';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider resetCSS theme={customTheme}>
+      <ColorModeProvider
+        options={{
+          initialColorMode: 'light',
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;

aa9d63823427741ff481b1c3f22e125819ce8698
Nguyễn Bảo Khang <rta.tester.khangnguyen>
2022-03-18 15:05:25 +0700

    create navbar.js

---
+++ components/dashboard/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <div>
+            <div className="navbar">
+                <Link to="#" className="menu-bars">
+                    
+                </Link>
+            </div>
+        </div>
+    );
+}

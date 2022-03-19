b63798f4a24b866e6ebccec4920e3deca3086b91
Andrew Le <s3820098@rmit.edu.vn>
2022-03-19 11:51:47 +0700

    An 4: add new resources

--- assets/images.js
+++ assets/images.js
@@ -1,7 +1,9 @@
 import MainLogo from './images/main-logo.png';
+import BusinessMan from './images/business-man.png';
 
 const ImageResources = {
   MainLogo,
+  BusinessMan,
 };
 
 export default ImageResources;

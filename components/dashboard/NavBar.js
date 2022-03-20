1f43469d9b9fc9d3c093d980923a6181fe8d18e6
Nguyễn Bảo Khang <rta.tester.khangnguyen@gmail.com>
2022-03-21 00:22:44 +0700

    Khang 2: update vertical navbar

--- components/dashboard/NavBar.js
+++ components/dashboard/NavBar.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import Avvvatars from 'avvvatars-react';
+import Link from 'next/link';
 
 function NavBar() {
+    const {sidebar, setSideBar} = useState(false)
+
+    const showSidebar = () => setSideBar(!sidebar)
     return (
         <div>
             <div className="navbar">
-                <Link to="#" className="menu-bars">
-                    
+                <Link href="#" className="menu-bars">
+                    <Avvvatars style="shape" value="Scrumified" />
                 </Link>
             </div>
+
+            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+                <ul className="nav-menu-items">
+                    <li className="navbar-toggle">
+                        <Link href="#" className="menu-bars">
+
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
+
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -2,77 +2,75 @@ import Image from 'next/image';
 import NextLink from 'next/link';
 import Head from 'next/head';
 
-import { Container, Flex, Text, Button, Link } from '@chakra-ui/react';
-import DarkModeSwitch from '../components/DarkModeSwitch';
-import ImageResrouces from '../assets/images';
+// import { Container, Flex, Text, Button, Link } from '@chakra-ui/react';
+import NavBar from '../components/dashboard/NavBar';
+// import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 export default function Home() {
-  const navContent = ['Home', 'Features', 'How it works', 'Testinomial'];
+  // const navContent = ['Home', 'Features', 'How it works', 'Testinomial'];
 
   return (
     <>
       <Head>
         <title>Home - Scrumified</title>
       </Head>
-      <Container maxW="5xl" p={0}>
-        <NavBar navContent={navContent} />
-      </Container>
+      <NavBar />
     </>
   );
 }
 
-const NavBar = ({ navContent }) => (
-  <Flex
-    alignItems="center"
-    justifyContent="space-between"
-    px={5}
-    py={3}
-    borderBottom="1px"
-    borderColor="gray.200"
-  >
-    {/* logo */}
-    <NextLink href="/" passHref>
-      <Link
-        _hover={{
-          color: '#4599fe',
-        }}
-        style={{ textDecoration: 'none' }}
-      >
-        <Flex alignItems="center">
-          <Image
-            src={ImageResrouces.MainLogo}
-            alt="App main logo"
-            width="50%"
-            height="50%"
-          />
+// const NavBar = ({ navContent }) => (
+//   <Flex
+//     alignItems="center"
+//     justifyContent="space-between"
+//     px={5}
+//     py={3}
+//     borderBottom="1px"
+//     borderColor="gray.200"
+//   >
+//     {/* logo */}
+//     <NextLink href="/" passHref>
+//       <Link
+//         _hover={{
+//           color: '#4599fe',
+//         }}
+//         style={{ textDecoration: 'none' }}
+//       >
+//         <Flex alignItems="center">
+//           <Image
+//             src={ImageResrouces.MainLogo}
+//             alt="App main logo"
+//             width="50%"
+//             height="50%"
+//           />
 
-          <Text fontSize="3xl" fontWeight="bold" color="#031e49">
-            Scrumified
-          </Text>
-        </Flex>
-      </Link>
-    </NextLink>
+//           <Text fontSize="3xl" fontWeight="bold" color="#031e49">
+//             Scrumified
+//           </Text>
+//         </Flex>
+//       </Link>
+//     </NextLink>
 
-    {/* nav content */}
-    <Flex alignItems="center" gap="12">
-      {navContent.map((textContent) => (
-        <Link
-          key={textContent}
-          _hover={{
-            color: '#4599fe',
-          }}
-          style={{ textDecoration: 'none' }}
-        >
-          <Text fontSize="sm">{textContent}</Text>
-        </Link>
-      ))}
-    </Flex>
+//     {/* nav content */}
+//     <Flex alignItems="center" gap="12">
+//       {navContent.map((textContent) => (
+//         <Link
+//           key={textContent}
+//           _hover={{
+//             color: '#4599fe',
+//           }}
+//           style={{ textDecoration: 'none' }}
+//         >
+//           <Text fontSize="sm">{textContent}</Text>
+//         </Link>
+//       ))}
+//     </Flex>
 
-    {/* buttons */}
-    <Flex alignItems="center" gap="5">
-      <DarkModeSwitch />
+//     {/* buttons */}
+//     <Flex alignItems="center" gap="5">
+//       <DarkModeSwitch />
 
-      <Button>GET STARTED</Button>
-    </Flex>
-  </Flex>
-);
+//       <Button>GET STARTED</Button>
+//     </Flex>
+//   </Flex>
+// );

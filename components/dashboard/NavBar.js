c7d0d70c3edde11862c5a382155a683d8c723c9a
Nguyễn Bảo Khang <rta.tester.khangnguyen>
2022-03-23 15:51:26 +0700

    Khang - 6 - update navbar styling

--- components/dashboard/NavBar.js
+++ components/dashboard/NavBar.js
@@ -1,59 +1,56 @@
 import React from 'react';
 import Avvvatars from 'avvvatars-react';
-import Link as NextLink} from 'next/link';
-import { Flex, useColorMode, useDisclosure } from '@chakra-ui/react';
-import { NavBarDataUp } from './NavBarDataUp';
-import { NavBarDataDown } from './NavBarDataDown';
+import {Link as NextLink} from 'next/link';
+// import { Link, Box, Center, Wrap, WrapItem, useDisclosure } from '@chakra-ui/react';
+import {
+    IconButton,
+    Box,
+    CloseButton,
+    Flex,
+    Icon,
+    useColorModeValue,
+    Link,
+    Drawer,
+    DrawerContent,
+    Text,
+    useDisclosure,
+    BoxProps,
+    FlexProps,
+    WrapItem,
+    Wrap,
+    
+  } from '@chakra-ui/react';
 
-function NavBar() {
+import { NavBarData } from './NavBarData';
+
+export default function NavBar() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-        <div>
-            <div className="navbar">
-                <Link href="#" className="menu-bars">
+        <Box bg='pink' h='100vh'>
+            <Box className="navbar">
+                <Link as={NextLink} href="#" className="menu-bars">
                     <Avvvatars value="Scrumified" />
                 </Link>
-            </div>
+            </Box>
 
-            <nav className="nav-menu-up">
-                <ul className="nav-menu-items">
-                    {NavBarDataUp.map((item, index) => {
+            <Box className="nav-menu-up" bg='teal' h='80vh'>
+                <Wrap className="nav-menu-items">
+                    {NavBarData.map((item, index) => {
                         return (
-                            <li key={index} className={item.name}>
-                                <Link href={item.path} className={item.name}>
-                                    {item.icon}
+                            <WrapItem key={index} className={item.name}>
+                                <Link as={NextLink} href={item.path} className={item.name}>
+                                    {item.icon} {item.title}
                                 </Link>
-                                <Link href={item.path} className={item.name}>
-                                    {item.title}
-                                </Link>
-                            </li>
+                            </WrapItem>
                         )
                     })}
-                </ul>
-            </nav>
-
-            <nav className='nav-menu-down'>
-                <ul className="nav-menu-items">
-                    {NavBarDataDown.map((item, index) => {
-                        return (
-                            <li key={index} className={item.name}>
-                                <Link href={item.path} className={item.name}>
-                                    {item.icon}
-                                </Link>
-                                <Link href={item.path} className={item.name}>
-                                    {item.title}
-                                </Link>
-                            </li>
-                        )
-                    })}
-                    <li className="profile">
+                    <WrapItem className="profile">
                         <Link href="/profile">
                             <Avvvatars value="Khang Nguyen" />
                         </Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+                    </WrapItem>
+                </Wrap>
+            </Box>
+        </Box>
     );
 }
-
-export default NavBar;
--- components/dashboard/NavBarData.js
+++ components/dashboard/NavBarData.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Icon } from '@chakra-ui/react'
-import { AiFillHome } from 'react-icons/ai';
-import { MdTimeline } from 'react-icons/md';
+import { AiFillHome,AiFillNotification,AiFillSetting } from 'react-icons/ai';
+import { MdTimeline,MdOutlineHelp } from 'react-icons/md';
 import { VscTasklist } from 'react-icons/vsc';
 import { GiSprint } from 'react-icons/gi';
 import { FaClipboard } from 'react-icons/fa';
 
-export const NavBarDataUp = [
+export const NavBarData = [
     {
         title: 'Home',
         path: './index',
@@ -35,6 +35,24 @@ export const NavBarDataUp = [
         title: 'Report',
         path: '/report',
         icon: <Icon as={FaClipboard} />,
+        name: 'nav-text'
+    },
+    {
+        title: 'Notification',
+        path: './notification',
+        icon: <Icon as={AiFillNotification} />,
+        name: 'nav-text'
+    },
+    {
+        title: 'Help',
+        path: '/help',
+        icon: <Icon as={MdOutlineHelp} />,
+        name: 'nav-text'
+    },
+    {
+        title: 'Setting',
+        path: '/setting',
+        icon: <Icon as={AiFillSetting} />,
         name: 'nav-text'
     }
 ]

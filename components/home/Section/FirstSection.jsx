88f62dc88ea915e492c47b775583d96d976923fa
thang.le <truongan1730@gmail.com>
2022-03-30 14:00:56 +0700

    An 38: minor: responsiveness at of first section right part

--- components/home/Section/FirstSection.jsx
+++ components/home/Section/FirstSection.jsx
@@ -1,4 +1,3 @@
-import NextLink from 'next/link';
 import {
   Flex,
   Image as ChakraImage,
@@ -11,7 +10,7 @@ import {
 } from '@chakra-ui/react';
 import Images from '../../../assets/images';
 import { motion, useAnimation } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 // integrate Chakra Flex with framer motion
 const MotionFlex = motion(Flex);
@@ -24,10 +23,10 @@ const FirstSection = ({ setIsRegistering, setTypedEmail, typedEmail }) => {
       bg={useColorModeValue('#fff', '#031e49')}
       as="section"
       px="2rem"
-      pt="4.5rem"
+      pt={{ base: 0, lg: '4.5rem' }}
       maxW="full"
       w="100vw"
-      minH="115vh"
+      minH={{ base: '100vh', lg: '115vh' }}
       style={{
         '-moz-user-select': 'none',
         '-khtml-user-select': 'none',
@@ -78,8 +77,8 @@ const LeftPart = () => {
           src={Images.PairStanding.src}
           alt="A standing pair"
           mr={{ base: 0, md: 0, lg: '2rem', xlg: '2rem' }}
-          width={{ base: 0, md: 0, lg: '310px', xlg: '400px' }}
-          height={{ base: 0, md: 0, lg: '520px', xlg: '650px' }}
+          width={{ base: 0, md: 0, lg: '310px', xlg: '350px' }}
+          height={{ base: 0, md: 0, lg: '520px', xlg: '580px' }}
           // priority
         />
       ) : (
@@ -87,8 +86,8 @@ const LeftPart = () => {
           src={Images.LeaningBusinessMan.src}
           alt="Leaning to right business man"
           mr={{ base: 0, md: 0, lg: '1rem', xlg: '2rem' }}
-          width={{ base: 0, md: 0, lg: '375px', xlg: '500px' }}
-          height={{ base: 0, md: 0, lg: '500px', xlg: '650px' }}
+          width={{ base: 0, md: 0, lg: '375px', xlg: '400px' }}
+          height={{ base: 0, md: 0, lg: '500px', xlg: '560px' }}
           // priority
         />
       )}

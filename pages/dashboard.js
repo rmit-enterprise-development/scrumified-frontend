29fccc54ec5dbe76fc22cc79a24cfd85b9654c17
Minh Pham <pcminh0505@gmail.com>
2022-04-15 16:44:33 +0700

    Minh 9: Implement axios API at Register
    Need to adjust prettier settings :))

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,23 +7,13 @@ import Sidebar from "../components/dashboard/SideBar/SideBar.tsx";
 import userAPI from "../api/services/userAPI";
 
 const Dashboard = () => {
-  // const { response, error, loading } = useAxios(userAPI.getOne(1));
-
-  // const { call } = useAxios(userAPI.getOne(1));
-
-  // if (loading) {
-  //   console.log("Loading...");
-  // } else {
-  //   console.log("Get data successfully");
-  //   console.log("Getting user with ID 1 response: ", response);
-  // }
   const [userList, setUserList] = useState([]);
 
   useEffect(() => {
     const fetchUserList = async () => {
       try {
         const response = await userAPI.getAll();
-        console.log(response);
+        setUserList(response);
       } catch (error) {
         console.log("Fail to fetch: ", error);
       }
@@ -32,29 +22,7 @@ const Dashboard = () => {
     fetchUserList();
   }, []);
 
-  const mockUser = {
-    firstName: "HoHo",
-    lastName: "Uncle",
-    email: "hochiminhmuonnam@gmail.com",
-    password: "dcmtestpasswordthoynha",
-  };
-
-  const registerUser = async (submitData) => {
-    // const requestOptions = {
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: submitData,
-    // };
-
-    console.log("submitData: ", submitData);
-    try {
-      const response = await userAPI.register(submitData);
-      console.log(response);
-    } catch (error) {
-      console.log(error.response.data);
-    }
-  };
+  console.log("userList: ", userList);
 
   return (
     <Box display="flex">
@@ -71,11 +39,6 @@ const Dashboard = () => {
           <ProjectGrid />
 
           <SectionHeader>Assigned to me</SectionHeader>
-
-          {/* Only for mock API user: Move this into other section later  */}
-          <Button onClick={() => registerUser(mockUser)}>
-            Click to register user
-          </Button>
         </Box>
       </Box>
     </Box>

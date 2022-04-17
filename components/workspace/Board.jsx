e5e675d471e8fd7f53cea9f9159f4cc85af47161
Minh Pham <pcminh0505@gmail.com>
2022-04-17 18:02:23 +0700

    [Hotfix] Overflow Sprint cards

--- components/workspace/Board.jsx
+++ components/workspace/Board.jsx
@@ -1,81 +1,75 @@
-import { Flex } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { DragDropContext } from "react-beautiful-dnd";
 
 const Board = ({ data, setData, children }) => {
-	const onDragEnd = (result) => {
-		const { destination, source, draggableId } = result;
-		if (!destination) {
-			return;
-		}
-		if (
-			destination.droppableId === source.droppableId &&
-			destination.index === source.index
-		) {
-			return;
-		}
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
 
-		console.log(destination, source, draggableId);
+    console.log(destination, source, draggableId);
 
-		if (destination.droppableId === source.droppableId) {
-			const newData = Array.from(data);
-			const sourceCard = newData.filter(
-				(card) => card.id == draggableId
-			)[0];
-			console.log(sourceCard);
-			newData.map((card) => {
-				if (card.status === destination.droppableId) {
-					if (source.index > destination.index) {
-						source.index >= card.position &&
-						card.position >= destination.index
-							? card.position++
-							: card.position;
-					} else {
-						source.index <= card.position &&
-						card.position <= destination.index
-							? card.position--
-							: card.position;
-					}
-				}
-			});
+    if (destination.droppableId === source.droppableId) {
+      const newData = Array.from(data);
+      const sourceCard = newData.filter((card) => card.id == draggableId)[0];
+      console.log(sourceCard);
+      newData.map((card) => {
+        if (card.status === destination.droppableId) {
+          if (source.index > destination.index) {
+            source.index >= card.position && card.position >= destination.index
+              ? card.position++
+              : card.position;
+          } else {
+            source.index <= card.position && card.position <= destination.index
+              ? card.position--
+              : card.position;
+          }
+        }
+      });
 
-			sourceCard.position = destination.index;
-			sourceCard.status = destination.droppableId;
+      sourceCard.position = destination.index;
+      sourceCard.status = destination.droppableId;
 
-			setData(newData);
-		} else {
-			const newData = Array.from(data);
-			const sourceCard = newData.filter(
-				(card) => card.id == draggableId
-			)[0];
+      setData(newData);
+    } else {
+      const newData = Array.from(data);
+      const sourceCard = newData.filter((card) => card.id == draggableId)[0];
 
-			newData.map((card) => {
-				if (
-					card.status === destination.droppableId &&
-					card.position >= destination.index
-				) {
-					card.position++;
-				} else if (
-					card.status === source.droppableId &&
-					card.position >= source.index
-				) {
-					card.position--;
-				}
-			});
+      newData.map((card) => {
+        if (
+          card.status === destination.droppableId &&
+          card.position >= destination.index
+        ) {
+          card.position++;
+        } else if (
+          card.status === source.droppableId &&
+          card.position >= source.index
+        ) {
+          card.position--;
+        }
+      });
 
-			sourceCard.position = destination.index;
-			sourceCard.status = destination.droppableId;
+      sourceCard.position = destination.index;
+      sourceCard.status = destination.droppableId;
 
-			setData(newData);
-		}
-	};
-	return (
-		<DragDropContext onDragEnd={onDragEnd}>
-			<Flex columnGap={4}>
-				{React.Children.map(children, (child) => child)}
-			</Flex>
-		</DragDropContext>
-	);
+      setData(newData);
+    }
+  };
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Flex columnGap={4}>
+        {React.Children.map(children, (child) => child)}
+      </Flex>
+    </DragDropContext>
+  );
 };
 
 export default Board;
--- components/workspace/Column.jsx
+++ components/workspace/Column.jsx
@@ -1,33 +1,37 @@
-import { Text, Box, Flex, Grid } from '@chakra-ui/react';
-import React from 'react';
-import { Droppable } from 'react-beautiful-dnd';
-import Card from './Card';
+import { Text, Box, Flex, Grid } from "@chakra-ui/react";
+import React from "react";
+import { Droppable } from "react-beautiful-dnd";
+import Card from "./Card";
 
 const Column = ({ title, id, cards }) => {
-	console.log(cards);
-	return (
-		<Flex flexDir={'column'} borderWidth={'4px'} w={'md'} dir="column">
-			<Text>{title}</Text>
-			<Droppable droppableId={id}>
-				{(provided) => (
-					<Box
-						flexGrow={1}
-						ref={provided.innerRef}
-						{...provided.droppableProps}
-					>
-						{cards.map((card) => (
-							<Card
-								key={card.id}
-								card={card}
-								index={card.position}
-							/>
-						))}
-						{provided.placeholder}
-					</Box>
-				)}
-			</Droppable>
-		</Flex>
-	);
+  console.log(cards);
+  return (
+    <Flex
+      flexDir={"column"}
+      borderWidth={"4px"}
+      dir="column"
+      alignItems={"center"}
+    >
+      <Text fontWeight={"bold"} py={1}>
+        {title}
+      </Text>
+      <Droppable droppableId={id}>
+        {(provided) => (
+          <Box
+            flexGrow={1}
+            mx={1}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {cards.map((card) => (
+              <Card key={card.id} card={card} index={card.position} />
+            ))}
+            {provided.placeholder}
+          </Box>
+        )}
+      </Droppable>
+    </Flex>
+  );
 };
 
 export default Column;

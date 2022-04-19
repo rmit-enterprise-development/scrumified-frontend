4b7ce7a9cfa9d72b8dfdb5f9b107afd988966195
Andrew Le <s3820098@rmit.edu.vn>
2022-04-19 19:32:04 +0700

    An 58: add placeholder for third section

--- components/home/Section/ThirdSection.jsx
+++ components/home/Section/ThirdSection.jsx
@@ -1,29 +1,110 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import {
-  Flex,
-  Box,
-  Text,
-  SimpleGrid,
-  Center,
-  useColorModeValue,
-  useBreakpointValue,
-} from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 
 const ThirdSection = () => {
-  useEffect(() => {
-    AOS.init({ duration: 1200, mirror: true });
-  }, []);
-  return (
-    <Flex
-      bg="cyan"
-      w="100vw"
-      h="100vh"
-      direction="column"
-      px={{ base: '1rem', md: '2rem' }}
-    ></Flex>
-  );
+    useEffect(() => {
+        AOS.init({ duration: 1200, mirror: true });
+    }, []);
+
+    return (
+        <>
+            <Flex
+                w="100vw"
+                h="100vh"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Text
+                    fontSize="30rem"
+                    textAlign="center"
+                    style={{
+                        background:
+                            'linear-gradient(22deg, rgba(235,5,70,0.9) 21%, rgba(69,153,254,0.9) 71%)',
+                        '-webkit-background-clip': 'text',
+                        '-webkit-text-fill-color': 'transparent',
+                    }}
+                >
+                    1
+                </Text>
+
+                <Flex bg="violet" h="full" w="35rem">
+                    dfdf
+                </Flex>
+            </Flex>
+
+            <Flex
+                w="100vw"
+                h="100vh"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Flex bg="violet" h="full" w="35rem">
+                    dfdf
+                </Flex>
+                <Text
+                    fontSize="30rem"
+                    textAlign="center"
+                    style={{
+                        background:
+                            'linear-gradient(22deg, rgba(235,5,70,0.9) 21%, rgba(69,153,254,0.9) 71%)',
+                        '-webkit-background-clip': 'text',
+                        '-webkit-text-fill-color': 'transparent',
+                    }}
+                >
+                    2
+                </Text>
+            </Flex>
+
+            <Flex
+                w="100vw"
+                h="100vh"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Text
+                    fontSize="30rem"
+                    textAlign="center"
+                    style={{
+                        background:
+                            'linear-gradient(22deg, rgba(235,5,70,0.9) 21%, rgba(69,153,254,0.9) 71%)',
+                        '-webkit-background-clip': 'text',
+                        '-webkit-text-fill-color': 'transparent',
+                    }}
+                >
+                    3
+                </Text>
+
+                <Flex bg="violet" h="full" w="35rem">
+                    dfdf
+                </Flex>
+            </Flex>
+
+            <Flex
+                w="100vw"
+                h="100vh"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Flex bg="violet" h="full" w="35rem">
+                    dfdf
+                </Flex>
+                <Text
+                    fontSize="30rem"
+                    textAlign="center"
+                    style={{
+                        background:
+                            'linear-gradient(22deg, rgba(235,5,70,0.9) 21%, rgba(69,153,254,0.9) 71%)',
+                        '-webkit-background-clip': 'text',
+                        '-webkit-text-fill-color': 'transparent',
+                    }}
+                >
+                    4
+                </Text>
+            </Flex>
+        </>
+    );
 };
 
 export default ThirdSection;

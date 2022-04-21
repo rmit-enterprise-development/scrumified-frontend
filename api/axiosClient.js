aa789955c7d969361692270af3f94dac0ea7a15b
Minh Pham <pcminh0505@gmail.com>
2022-04-21 14:38:36 +0700

    [Fix] Update new api client

--- api/axiosClient.js
+++ api/axiosClient.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import queryString from 'query-string';
+import axios from "axios";
+import queryString from "query-string";
 
 // Set up default config for http requests here
 // Please have a look at here `https://github.com/axios/axios#request- config` for the full list of configs
 const axiosClient = axios.create({
-    baseURL: 'https://scrumified-api.herokuapp.com/',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    paramsSerializer: (params) => queryString.stringify(params),
+  baseURL: "https://scrumified-dev-bakend.herokuapp.com/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
 });
 
 // axiosClient.interceptors.request.use(async (config) => {
@@ -17,13 +17,13 @@ const axiosClient = axios.create({
 // });
 
 axiosClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        // Handle errors
-        throw error;
-    }
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // Handle errors
+    throw error;
+  }
 );
 
 export default axiosClient;

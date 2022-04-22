38613ec45275d6f5fe4a3aaae119d10678401477
Minh Pham <pcminh0505@gmail.com>
2022-04-22 12:48:07 +0700

    Minh 21: Mock API create project

--- components/dashboard/CreateProjectModal/CreateProjectModal.jsx
+++ components/dashboard/CreateProjectModal/CreateProjectModal.jsx
@@ -1,168 +1,182 @@
-import { AddIcon } from '@chakra-ui/icons';
-// import matchSorter from "match-sorter";
+import { AddIcon } from "@chakra-ui/icons";
 import {
-	Button,
-	Flex,
-	FormControl,
-	FormLabel,
-	Input,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	ModalOverlay,
-	Text,
-	useColorMode,
-	useColorModeValue,
-	useDisclosure,
-} from '@chakra-ui/react';
-import Avvvatars from 'avvvatars-react';
-import { CUIAutoComplete } from 'chakra-ui-autocomplete';
-import { useRef, useState } from 'react';
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+import Avvvatars from "avvvatars-react";
+import { CUIAutoComplete } from "chakra-ui-autocomplete";
+import Router from "next/router";
+import { useEffect, useRef, useState } from "react";
 
 const CreateProjectModal = ({ participantList }) => {
-	const { colorMode } = useColorMode();
-	const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-	const initialRef = useRef();
-	const finalRef = useRef();
+  const initialRef = useRef();
+  const finalRef = useRef();
 
-	console.log(participantList);
+  const [pickerItems, setPickerItems] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
+  console.log("selectedItems: ", selectedItems);
 
-	const [pickerItems, setPickerItems] = useState(
-		participantList.sort((a, b) => a.label.localeCompare(b.label))
-	);
+  const handleSelectedItemsChange = (selectedItems) => {
+    if (selectedItems) {
+      setSelectedItems(selectedItems);
+    }
+  };
 
-	const [selectedItems, setSelectedItems] = useState([]);
+  const customRender = (selected) => {
+    return (
+      <Flex flexDir="row" alignItems="center">
+        <Avvvatars value={selected.label} />
 
-	const handleSelectedItemsChange = (selectedItems) => {
-		if (selectedItems) {
-			setSelectedItems(selectedItems);
-		}
-	};
+        {colorMode === "dark" ? (
+          <Text pl={5} color="#fffdfe">
+            {selected.label}
+          </Text>
+        ) : (
+          <Text pl={5} color="#031d46">
+            {selected.label}
+          </Text>
+        )}
+      </Flex>
+    );
+  };
 
-	const customRender = (selected) => {
-		return (
-			<Flex flexDir="row" alignItems="center">
-				<Avvvatars value={selected.label} />
+  const customCreateItemRender = (value) => {
+    return (
+      <Text as="span" color="red.500" fontWeight="bold">
+        User not found!
+      </Text>
+    );
+  };
 
-				{colorMode === 'dark' ? (
-					<Text pl={5} color="#fffdfe">
-						{selected.label}
-					</Text>
-				) : (
-					<Text pl={5} color="#031d46">
-						{selected.label}
-					</Text>
-				)}
-			</Flex>
-		);
-	};
+  const handleSubmit = async () => {
+    const userID = "";
+    const request = {
+      title: "",
+      participantsId: selectedItems.map((a) => a.id),
+    };
+    try {
+      const response = await projectAPI.postProject(userID, request);
+      if (response && response.header !== 200) {
+        // Handle error
+      } else {
+        // Push to project backlog with new ID
+        const projectID = "";
+        Router.push({
+          pathname: `${RouterPage.PROJECT}/${projectID}${RouterPage.BACKLOG}`,
+        });
+      }
+    } catch (error) {
+      console.error("There was an error: ", error);
+    }
+  };
 
-	const customCreateItemRender = (value) => {
-		return (
-			<Text as="span" color="red.500" fontWeight="bold">
-				User not found!
-			</Text>
-		);
-	};
+  useEffect(() => {
+    setPickerItems(
+      participantList.sort((a, b) => a.label.localeCompare(b.label))
+    );
+  }, [participantList]);
 
-	return (
-		<>
-			<Button
-				leftIcon={<AddIcon />}
-				size="sm"
-				colorScheme="teal"
-				onClick={onOpen}
-			>
-				Create Project
-			</Button>
+  return (
+    <>
+      <Button
+        leftIcon={<AddIcon />}
+        size="sm"
+        colorScheme="teal"
+        onClick={onOpen}
+      >
+        Create Project
+      </Button>
 
-			<Modal
-				initialFocusRef={initialRef}
-				finalFocusRef={finalRef}
-				isOpen={isOpen}
-				onClose={onClose}
-			>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader
-						color={useColorModeValue('#031e49', '#fffdfe')}
-					>
-						Create your project
-					</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody pb={6}>
-						<FormControl isRequired>
-							<FormLabel
-								color={useColorModeValue('#031e49', '#fffdfe')}
-							>
-								Project name
-							</FormLabel>
-							<Input
-								ref={initialRef}
-								placeholder="Project name"
-								color={useColorModeValue('#031d46', '#fffdfe')}
-							/>
-						</FormControl>
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader color={useColorModeValue("#031e49", "#fffdfe")}>
+            Create your project
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl isRequired>
+              <FormLabel color={useColorModeValue("#031e49", "#fffdfe")}>
+                Project name
+              </FormLabel>
+              <Input
+                ref={initialRef}
+                placeholder="Project name"
+                color={useColorModeValue("#031d46", "#fffdfe")}
+              />
+            </FormControl>
 
-						<FormControl mt={4} isRequired>
-							<CUIAutoComplete
-								tagStyleProps={{
-									rounded: 'full',
-								}}
-								label="Participants"
-								placeholder="Enter a participant's email"
-								onCreateItem={() => {}} // Empty because don't want to add option in list. Please see the example in "https://www.npmjs.com/package/chakra-ui-autocomplete"
-								items={pickerItems}
-								itemRenderer={customRender}
-								createItemRenderer={customCreateItemRender}
-								selectedItems={selectedItems}
-								onSelectedItemsChange={(changes) =>
-									handleSelectedItemsChange(
-										changes.selectedItems
-									)
-								}
-								hideToggleButton={true}
-								listStyleProps={{
-									maxHeight: '200',
-									overflow: 'auto',
-									bg: useColorModeValue('', '#2D3748'),
-								}}
-								listItemStyleProps={{
-									cursor: 'pointer',
-									_hover: {
-										bg: useColorModeValue('', '#031e49'),
-									},
-								}}
-								labelStyleProps={{
-									color: useColorModeValue(
-										'#031e49',
-										'#fffdfe'
-									),
-								}}
-								inputStyleProps={{
-									color: useColorModeValue(
-										'#031d46',
-										'#fffdfe'
-									),
-								}}
-							/>
-						</FormControl>
-					</ModalBody>
+            <FormControl mt={4} isRequired>
+              <CUIAutoComplete
+                tagStyleProps={{
+                  rounded: "full",
+                }}
+                label="Participants"
+                placeholder="Enter a participant's email"
+                onCreateItem={() => {}} // Empty because don't want to add option in list. Please see the example in "https://www.npmjs.com/package/chakra-ui-autocomplete"
+                items={pickerItems}
+                itemRenderer={customRender}
+                createItemRenderer={customCreateItemRender}
+                selectedItems={selectedItems}
+                onSelectedItemsChange={(changes) =>
+                  handleSelectedItemsChange(changes.selectedItems)
+                }
+                hideToggleButton={true}
+                listStyleProps={{
+                  maxHeight: "200",
+                  overflow: "auto",
+                  bg: useColorModeValue("", "#2D3748"),
+                }}
+                listItemStyleProps={{
+                  cursor: "pointer",
+                  _hover: {
+                    bg: useColorModeValue("", "#031e49"),
+                  },
+                }}
+                labelStyleProps={{
+                  color: useColorModeValue("#031e49", "#fffdfe"),
+                }}
+                inputStyleProps={{
+                  color: useColorModeValue("#031d46", "#fffdfe"),
+                }}
+              />
+            </FormControl>
+          </ModalBody>
 
-					<ModalFooter>
-						<Button colorScheme="blue" mr={3}>
-							Save
-						</Button>
-						<Button onClick={onClose}>Cancel</Button>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
-		</>
-	);
+          <ModalFooter>
+            <Button onClick={onClose} mr={3}>
+              Cancel
+            </Button>
+            <Button onClick={handleSubmit} colorScheme="blue">
+              Create
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default CreateProjectModal;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,79 +1,81 @@
-import { Box, Flex } from '@chakra-ui/react';
-import Head from 'next/head';
-import { useState } from 'react';
-import SectionHeader from '../components/common/SectionHeader/SectionHeader';
-import CreateProjectModal from '../components/dashboard/CreateProjectModal/CreateProjectModal';
-import ProjectGrid from '../components/dashboard/ProjectGrid/ProjectGrid';
-import MainContainer from '../components/layout/MainContainer';
+import { Flex } from "@chakra-ui/react";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import userAPI from "../api/services/userAPI";
+import SectionHeader from "../components/common/SectionHeader/SectionHeader";
+import CreateProjectModal from "../components/dashboard/CreateProjectModal/CreateProjectModal";
+import ProjectGrid from "../components/dashboard/ProjectGrid/ProjectGrid";
+import MainContainer from "../components/layout/MainContainer";
+import { digFind } from "../utils/object";
 
 const Dashboard = () => {
-	// useEffect(() => {
-	//   const fetchUserList = async () => {
-	//     try {
-	//       const response = await userAPI.getAll();
-	//       console.log("response: ", response);
+  const [userList, setUserList] = useState([]);
+  // {
+  //   id: "1",
+  //   name: "Minh Pham",
+  //   email: "pcminh0505@gmail.com",
+  // },
+  // {
+  //   id: "3",
+  //   name: "Thach Ho",
+  //   email: "thachho@123@gmail.com",
+  // },
+  // {
+  //   id: "2",
+  //   name: "Khang Nguyen",
+  //   email: "khangnguyen111101@gmail.com",
+  // },
+  // {
+  //   id: "5",
+  //   name: "Duong Nguyen",
+  //   email: "duongnguyen123@gmail.com",
+  // },
+  // {
+  //   id: "4",
+  //   name: "An Le",
+  //   email: "andrew123@gmail.com",
+  // },
+  // ]);
 
-	//       setUserList(response);
-	//     } catch (error) {
-	//       console.log("Fail to fetch: ", error);
-	//     }
-	//   };
+  useEffect(() => {
+    const fetchUserList = async () => {
+      try {
+        const response = await userAPI.getAll();
+        const data = digFind(response, "userDtoList");
 
-	//   fetchUserList();
-	// }, []);
+        setUserList(data);
+      } catch (error) {
+        console.log("Fail to fetch: ", error);
+      }
+    };
 
-	// console.log("userList: ", userList);
+    fetchUserList();
+  }, []);
 
-	const [userList, setUserList] = useState([
-		{
-			id: '1',
-			name: 'Minh Pham',
-			email: 'pcminh0505@gmail.com',
-		},
-		{
-			id: '3',
-			name: 'Thach Ho',
-			email: 'thachho@123@gmail.com',
-		},
-		{
-			id: '2',
-			name: 'Khang Nguyen',
-			email: 'khangnguyen111101@gmail.com',
-		},
-		{
-			id: '5',
-			name: 'Duong Nguyen',
-			email: 'duongnguyen123@gmail.com',
-		},
-		{
-			id: '4',
-			name: 'An Le',
-			email: 'andrew123@gmail.com',
-		},
-	]);
-	return (
-		<>
-			<Head>
-				<title>Dashboard</title>
-			</Head>
-			<MainContainer>
-				<Flex justifyContent="space-between" alignItems="center">
-					<SectionHeader>My Projects</SectionHeader>
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <MainContainer>
+        <Flex justifyContent="space-between" alignItems="center">
+          <SectionHeader>My Projects</SectionHeader>
 
-					<CreateProjectModal
-						participantList={userList.map((a) => {
-							const userInfo = a.name + ' (' + a.email + ')';
-							return { value: a.id, label: userInfo };
-						})}
-					/>
-				</Flex>
+          <CreateProjectModal
+            participantList={userList.map((a) => {
+              const userInfo =
+                a.firstName + " " + a.lastName + " (" + a.email + ")";
+              return { value: a.id, label: userInfo };
+            })}
+          />
+        </Flex>
 
-				<ProjectGrid />
+        <ProjectGrid />
 
-				<SectionHeader>Assigned to me</SectionHeader>
-			</MainContainer>
-		</>
-	);
+        <SectionHeader>Assigned to me</SectionHeader>
+      </MainContainer>
+    </>
+  );
 };
 
 export default Dashboard;

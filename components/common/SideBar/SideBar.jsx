9ee1b39078a1a0158076add0830f1ac309acdf01
Minh Pham <pcminh0505@gmail.com>
2022-04-25 16:53:52 +0700

    [Refractor] UseContext to get current user

--- components/common/SideBar/SideBar.jsx
+++ components/common/SideBar/SideBar.jsx
@@ -12,14 +12,13 @@ import React from "react";
 import { FiMenu } from "react-icons/fi";
 import { SidebarContent } from "./SidebarContent";
 
-export default function Sidebar({ user, children }) {
+export default function Sidebar({ children }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { toggleColorMode } = useColorMode();
 
   return (
     <Box minH="100vh" bg={useColorModeValue("#FFFDFE", "#031E49")}>
       <SidebarContent
-        user={user}
         onClose={() => onClose}
         toggleColorMode={toggleColorMode}
         display={{ base: "none", md: "block" }}

ef410a8d8df874532cd9d0ea6ae439ee70daa388
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2022-05-12 09:12:15 +0700

    Update sprint.js

--- pages/project/[id]/sprint.js
+++ pages/project/[id]/sprint.js
@@ -1,116 +1,81 @@
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import SectionHeader from "../../../components/common/SectionHeader/SectionHeader";
-import MainContainer from "../../../components/layout/MainContainer";
-import Board from "../../../components/workspace/Board";
-import Column from "../../../components/workspace/Column";
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import SectionHeader from '../../../components/common/SectionHeader/SectionHeader';
+import MainContainer from '../../../components/layout/MainContainer';
+import Board from '../../../components/workspace/Board';
+import Column from '../../../components/workspace/Column';
 
-import cookies from "next-cookies";
-import { LoggedUserProvider } from "../../../components/common/LoggedUserProvider";
+import cookies from 'next-cookies';
+import { LoggedUserProvider } from '../../../components/common/LoggedUserProvider';
+import { useColorModeValue } from '@chakra-ui/react';
+
+const initData = [];
 
 const Sprint = ({ authToken }) => {
-  const initData = [
-    {
-      id: "1",
-      userStory: "Card1",
-      category: "Hello",
-      point: "12",
-      position: 2,
-      status: "todo",
-    },
-    {
-      id: "2",
-      userStory: "Card2",
-      category: "Hello",
-      point: "12",
-      position: 0,
-      status: "inProgress",
-    },
-    {
-      id: "3",
-      userStory: "Card3",
-      category: "Hello",
-      point: "12",
-      position: 0,
-      status: "done",
-    },
-    {
-      id: "4",
-      userStory: "Card4",
-      category: "Hello",
-      point: "12",
-      position: 1,
-      status: "todo",
-    },
-    {
-      id: "5",
-      userStory: "Card5",
-      category: "Hello",
-      point: "12",
-      position: 1,
-      status: "inProgress",
-    },
-    {
-      id: "6",
-      userStory: "Card6",
-      category: "Hello",
-      point: "12",
-      position: 0,
-      status: "todo",
-    },
-  ];
+	let bg = useColorModeValue('white', '#405A7D');
+	let color = useColorModeValue('#031d46', '#fffdfe');
+	let btnBg = useColorModeValue('gray.200', '#fffdfe');
+	let btnColor = 'black';
+	let bgGradient = useColorModeValue(
+		'linear(gray.50 0%, gray.100 100%)',
+		'linear(blue.800 0%, blue.900 100%)'
+	);
 
-  const [data, setData] = useState(initData);
+	const [data, setData] = useState(initData);
 
-  const [winReady, setwinReady] = useState(false);
-  useEffect(() => {
-    setwinReady(true);
-  }, []);
+	const [winReady, setwinReady] = useState(false);
+	useEffect(() => {
+		setwinReady(true);
+	}, []);
 
-  const filterCards = (s) => {
-    const cards = data.filter((card) => card.status === s);
-    cards = cards.sort((a, b) => a.position - b.position);
-    return cards;
-  };
+	const filterCards = (s) => {
+		const cards = data.filter((card) => card.status === s);
+		cards = cards.sort((a, b) => a.position - b.position);
+		return cards;
+	};
 
-  return (
-    <LoggedUserProvider authToken={authToken}>
-      <Head>
-        <title>Active Sprint</title>
-      </Head>
+	return (
+		<LoggedUserProvider authToken={authToken}>
+			<Head>
+				<title>Active Sprint</title>
+			</Head>
 
-      <MainContainer>
-        <SectionHeader>Active Sprint</SectionHeader>
-        {winReady ? (
-          <Board data={data} setData={setData} templateColumns="repeat(3, 1fr)">
-            <Column
-              key={0}
-              title={"Todo"}
-              id={"todo"}
-              cards={filterCards("todo")}
-            />
-            <Column
-              key={1}
-              title={"In Progress"}
-              id={"inProgress"}
-              cards={filterCards("inProgress")}
-            />
-            <Column
-              key={2}
-              title={"Done"}
-              id={"done"}
-              cards={filterCards("done")}
-            />
-          </Board>
-        ) : null}
-      </MainContainer>
-    </LoggedUserProvider>
-  );
+			<MainContainer>
+				<SectionHeader>Active Sprint</SectionHeader>
+				{winReady ? (
+					<Board
+						data={data}
+						setData={setData}
+						templateColumns="repeat(3, 1fr)"
+					>
+						<Column
+							key={0}
+							title={'Todo'}
+							id={'todo'}
+							cards={filterCards('todo')}
+						/>
+						<Column
+							key={1}
+							title={'In Progress'}
+							id={'inProgress'}
+							cards={filterCards('inProgress')}
+						/>
+						<Column
+							key={2}
+							title={'Done'}
+							id={'done'}
+							cards={filterCards('done')}
+						/>
+					</Board>
+				) : null}
+			</MainContainer>
+		</LoggedUserProvider>
+	);
 };
 
 export async function getServerSideProps(ctx) {
-  const { auth } = cookies(ctx);
-  return { props: { authToken: auth || "" } };
+	const { auth } = cookies(ctx);
+	return { props: { authToken: auth || '' } };
 }
 
 export default Sprint;

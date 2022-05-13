58455638d8dd2e6fd6036238b642c3322e8fe655
Nguyễn Bảo Khang <rta.tester.khangnguyen>
2022-05-13 16:05:19 +0700

    update gantt chart

--- components/roadmap/GanttChart.jsx
+++ components/roadmap/GanttChart.jsx
@@ -1,35 +1,39 @@
 import { Chart } from 'react-google-charts';
 import React from 'react';
-import { data } from './ChartData'
 import { useColorModeValue } from "@chakra-ui/react";
 
-const GanttChart = () => {
-    const options = {
-        height: 40*13,
-        gantt: {
-          trackHeight: 40,
-          criticalPathEnabled: false,
-          innerGridTrack: { fill: useColorModeValue("","#031d46") },
+const GanttChart = ({data}) => {
+  const chartHeight = data ? data.length * 40 : 500;
+  const options = {
+      height: chartHeight,
+      gantt: {
+        labelStyle: {
+          fontName: 'Arial',
+          fontSize: "md"
         },
-        backgroundColor: {
-          fill: useColorModeValue("#FFFDFE", "#031E49")
-        },
-        legend: {
-          textStyle: {
-            
-          }
+        trackHeight: 40,
+        criticalPathEnabled: false,
+        innerGridTrack: { fill: useColorModeValue("white","#031d46") },
+      },
+      backgroundColor: {
+        fill: useColorModeValue("white", 'yellow')
+      },
+      hAxis: {
+        titleTextStyle: {
+          color: "red"
         }
-    };
+      }
+  };
 
-    return (
-        <Chart
-        chartType="Gantt"
-        width="100%"
-        height="50%"
-        data={data}
-        options={options}
-      />
-    );
+  return (
+      <Chart
+      chartType="Gantt"
+      width="100%"
+      height="100%"
+      data={data}
+      options={options}
+    />
+  );
 };
 
 export default GanttChart;
--- pages/project/[id]/roadmap.js
+++ pages/project/[id]/roadmap.js
@@ -2,11 +2,156 @@ import Head from "next/head";
 import SectionHeader from "../../../components/common/SectionHeader/SectionHeader";
 import MainContainer from "../../../components/layout/MainContainer";
 import GanttChart from "../../../components/roadmap/GanttChart";
-
+import projectAPI from "../../../api/services/projectAPI";
 import cookies from "next-cookies";
 import { LoggedUserProvider } from "../../../components/common/LoggedUserProvider";
+import { useRouter } from 'next/router';
+
+const rows = [
+  [ 
+    "2014Spring",
+    "Spring 2014",
+    "dep trai",
+    new Date(2014, 2, 22),
+    new Date(2014, 5, 20),
+    null,
+    100,
+    null,
+  ],
+  [
+    "2014Summer",
+    "Summer 2014",
+    "summer",
+    new Date(2014, 5, 21),
+    new Date(2014, 8, 20),
+    null,
+    100,
+    null,
+  ],
+  [
+    "2014Autumn",
+    "Autumn 2014",
+    "autumn",
+    new Date(2014, 8, 21),
+    new Date(2014, 11, 20),
+    null,
+    100,
+    null,
+  ],
+  [
+    "2014Winter",
+    "Winter 2014",
+    "winter",
+    new Date(2014, 11, 21),
+    new Date(2015, 2, 21),
+    null,
+    100,
+    null,
+  ],
+  [
+    "2015Spring",
+    "Spring 2015",
+    "spring",
+    new Date(2015, 2, 22),
+    new Date(2015, 5, 20),
+    null,
+    50,
+    null,
+  ],
+  [
+    "2015Summer",
+    "Summer 2015",
+    "summer",
+    new Date(2015, 5, 21),
+    new Date(2015, 8, 20),
+    null,
+    0,
+    null,
+  ],
+  [
+    "2015Autumn",
+    "Autumn 2015",
+    "autumn",
+    new Date(2015, 8, 21),
+    new Date(2015, 11, 20),
+    null,
+    0,
+    null,
+  ],
+  [
+    "2015Winter",
+    "Winter 2015",
+    "winter",
+    new Date(2015, 11, 21),
+    new Date(2016, 2, 21),
+    null,
+    0,
+    null,
+  ],
+  [
+    "Football",
+    "Football Season",
+    "sports",
+    new Date(2014, 8, 4),
+    new Date(2015, 1, 1),
+    null,
+    100,
+    null,
+  ],
+  [
+    "Baseball",
+    "Baseball Season",
+    "sports",
+    new Date(2015, 2, 31),
+    new Date(2015, 9, 20),
+    null,
+    14,
+    null,
+  ],
+  [
+    "Basketball",
+    "Basketball Season",
+    "sports",
+    new Date(2014, 9, 28),
+    new Date(2015, 5, 20),
+    null,
+    86,
+    null,
+  ],
+  [
+    "Hockey",
+    "Hockey Season",
+    "sports",
+    new Date(2014, 9, 8),
+    new Date(2015, 5, 21),
+    null,
+    89,
+    null,
+  ],
+];
 
 const Roadmap = ({ authToken }) => {
+  const columns = [
+    { type: "string", label: "Task ID" },
+    { type: "string", label: "Task Name" },
+    { type: "string", label: "Resource" },
+    { type: "date", label: "Start Date" },
+    { type: "date", label: "End Date" },
+    { type: "number", label: "Duration" },
+    { type: "number", label: "Percentage done" },
+    { type: "string", lable: "Dependencies"}
+  ];
+
+  const { asPath } = useRouter();
+  const projectId = asPath.split('/')[2];
+
+  const sprintData = async () => {
+    const response = await projectAPI.getAllSprints(projectId);
+    console.log(projectId)
+    console.log(response)
+  };
+
+
   return (
     <LoggedUserProvider authToken={authToken}>
       <Head>
@@ -15,7 +160,7 @@ const Roadmap = ({ authToken }) => {
 
       <MainContainer>
         <SectionHeader>Project Roadmap</SectionHeader>
-        <GanttChart />
+        <GanttChart data={[columns, ...rows]} />
       </MainContainer>
     </LoggedUserProvider>
   );

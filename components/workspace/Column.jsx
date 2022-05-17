54db975edbae45109ee8c00eaac7089298971a88
Andrew Le <s3820098@rmit.edu.vn>
2022-05-17 20:38:24 +0700

    An 90: fix backlog scrolling

--- components/workspace/Column.jsx
+++ components/workspace/Column.jsx
@@ -1,8 +1,8 @@
-import { Flex, Skeleton, Tag, Text, useColorModeValue } from "@chakra-ui/react";
-import { Droppable } from "react-beautiful-dnd";
-import { GoTasklist } from "react-icons/go";
-import { AiOutlineStop } from "react-icons/ai";
-import NoItem from "../common/NoItem/NoItem";
+import { Flex, Skeleton, Tag, Text, useColorModeValue } from '@chakra-ui/react';
+import { Droppable } from 'react-beautiful-dnd';
+import { GoTasklist } from 'react-icons/go';
+import { AiOutlineStop } from 'react-icons/ai';
+import NoItem from '../common/NoItem/NoItem';
 
 const Column = ({
   title,
@@ -13,18 +13,18 @@ const Column = ({
   sprintStatus,
   isDragDisabled,
 }) => {
-  let color = useColorModeValue("#031d46", "#fffdfe");
+  let color = useColorModeValue('#031d46', '#fffdfe');
   return (
     <Flex
-      flexDir={"column"}
+      flexDir={'column'}
       dir="column"
       boxSizing="border-box"
       bgGradient={useColorModeValue(
-        "linear(gray.50 0%, gray.100 100%)",
-        "linear(blue.800 0%, blue.900 100%)"
+        'linear(gray.50 0%, gray.100 100%)',
+        'linear(blue.800 0%, blue.900 100%)'
       )}
       boxShadow="base"
-      borderRadius={"1rem"}
+      borderRadius={'1rem'}
       py={2.5}
       transition="all 0.5s linear"
       px={4}
@@ -33,11 +33,11 @@ const Column = ({
       minW="300px"
       // overflow="auto"
     >
-      <Flex alignItems={"center"}>
+      <Flex alignItems={'center'}>
         <Text
-          textAlign={"center"}
-          fontSize={"1.5rem"}
-          fontWeight={"bold"}
+          textAlign={'center'}
+          fontSize={'1.5rem'}
+          fontWeight={'bold'}
           color={color}
           p={4}
         >
@@ -45,49 +45,46 @@ const Column = ({
         </Text>
 
         <Tag
-          textAlign={"center"}
-          borderRadius={"0.5rem"}
-          size={"6"}
-          px={"6px"}
-          py={"4px"}
+          textAlign={'center'}
+          borderRadius={'0.5rem'}
+          size={'6'}
+          px={'6px'}
+          py={'4px'}
           bgColor={columnColor}
-          color={"white"}
+          color={'white'}
         >
           {cardList.length === 1 ? `1 card` : `${cardList.length} cards`}
         </Tag>
       </Flex>
 
-      <Skeleton isLoaded={!isLoading} h="100%">
-        <Droppable droppableId={id} isDropDisabled={isDragDisabled}>
-          {(provided) => (
-            <Flex
-              flexDirection={"column"}
-              flexGrow={1}
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              padding={"2"}
-              // overflow="auto"
-              // h="20rem"
-            >
-              {cardList &&
-                cardList.length === 0 &&
-                (isDragDisabled ? (
-                  <NoItem icon={AiOutlineStop}>
-                    Please start sprint to drag and drop
-                  </NoItem>
-                ) : sprintStatus === "inProgress" ? (
-                  <></>
-                ) : (
-                  <NoItem icon={GoTasklist}>
-                    No item found. Create some inside backlog!
-                  </NoItem>
-                ))}
-              {cardList}
-              {provided.placeholder}
-            </Flex>
-          )}
-        </Droppable>
-      </Skeleton>
+      <Droppable droppableId={id} isDropDisabled={isDragDisabled}>
+        {(provided) => (
+          <Flex
+            flexDirection={'column'}
+            flexGrow={1}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            padding={'2'}
+            overflow="scroll"
+          >
+            {cardList &&
+              cardList.length === 0 &&
+              (isDragDisabled ? (
+                <NoItem icon={AiOutlineStop}>
+                  Please start sprint to drag and drop
+                </NoItem>
+              ) : sprintStatus === 'inProgress' ? (
+                <></>
+              ) : (
+                <NoItem icon={GoTasklist}>
+                  No item found. Create some inside backlog!
+                </NoItem>
+              ))}
+            {cardList}
+            {provided.placeholder}
+          </Flex>
+        )}
+      </Droppable>
     </Flex>
   );
 };
